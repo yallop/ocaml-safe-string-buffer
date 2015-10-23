b1c9aafcf1be4fc7508f4c1cc8d3e590688abbc6
Jeremy Yallop <yallop@gmail.com>
2015-10-23 14:10:15 +0100

    Implement Immutable, and implement Mutable in terms of Immutable.

--- lib/aobuffer.ml
+++ lib/aobuffer.ml
@@ -28,8 +28,86 @@ struct
       Not_string -> ()
 end
 
-module Immutable =
+module Immutable :
+sig
+  type t
+
+  val empty : t
+
+  val contents : t -> string
+
+  val to_bytes : t -> bytes
+
+  (* val sub : t -> int -> int -> string *)
+
+  (* val blit : t -> int -> bytes -> int -> int -> unit *)
+
+  (* val nth : t -> int -> char *) (* TODO *)
+
+  val length : t -> int
+
+  val add_char : t -> char -> t
+
+  val add_string : t -> string -> t
+
+  val add_bytes : t -> bytes -> t
+
+  val add_substring : t -> string -> int -> int -> t
+
+  val add_subbytes : t -> bytes -> int -> int -> t
+
+  val add_buffer : t -> t -> t
+
+  (* val add_channel : t -> in_channel -> int -> unit *)
+
+  val output_buffer : out_channel -> t -> unit
+end =
 struct
+  (* Kept in reversed order *)
+  
+  (* TODO: add a string Weak.t option cache.  We could even add a
+     cache at each link in the chain. *)
+  type t = string list
+
+  let empty = []
+
+  let add_string buf s = s :: buf
+
+  let length buf =
+    List.fold_left (fun n s -> n + String.length s) 0 buf
+
+  let add_bytes buf b = add_string buf (Bytes.to_string b)
+
+  let add_char buf c = add_string buf (String.make 1 c)
+
+  let add_subbytes buf b ofs len =
+    add_string buf (Bytes.sub_string b ofs len)
+
+  let add_substring buf b ofs len =
+    add_string buf (String.sub b ofs len)
+
+  let to_bytes buf =
+    let len = length buf in
+    let b = Bytes.create len in
+    let _ = List.fold_left
+        (fun i s ->
+           let l = String.length s in
+           Bytes.blit_string s 0 b (i - l) l;
+           i - 1)
+        len
+        buf
+    in
+    b
+
+  let contents buf = Bytes.unsafe_to_string (to_bytes buf)
+
+  let iter_elements f elements =
+    List.fold_right (fun elem () -> ignore (f elem)) elements ()
+
+  let output_buffer outch elements =
+    iter_elements (output_string outch) elements
+
+  let add_buffer = (@)
 end
 
 module Mutable :
@@ -69,22 +147,20 @@ sig
   val output_buffer : out_channel -> t -> unit
 end =
 struct
-  (* Kept in reversed order *)
+  type t = { mutable elements: Immutable.t }
 
-  (* TODO: add a string Weak.t option cache.  We could even add a cache at
-     each link in the chain. *)
-  type t = { mutable elements: string list }
+  let create _ = { elements = Immutable.empty }
 
-  let create _ = { elements = [] }
+  let add_string buf s =
+    buf.elements <- Immutable.add_string buf.elements s
 
-  let add_string buf s = buf.elements <- s :: buf.elements
+  let add_bytes buf b =
+    buf.elements <- Immutable.add_bytes buf.elements b
 
-  let add_bytes buf b = add_string buf (Bytes.to_string b)
+  let add_char buf c =
+    buf.elements <- Immutable.add_char buf.elements c
 
-  let add_char buf c = add_string buf (String.make 1 c)
-
-  let length { elements } =
-    List.fold_left (fun n s -> n + String.length s) 0 elements
+  let length { elements } = Immutable.length elements
 
   let add_subbytes buf b ofs len =
     add_string buf (Bytes.sub_string b ofs len)
@@ -92,37 +168,18 @@ struct
   let add_substring buf b ofs len =
     add_string buf (String.sub b ofs len)
 
-  let clear buf = buf.elements <- []
+  let clear buf = buf.elements <- Immutable.empty
 
-  let iter_elements f elements =
-    List.fold_right (fun elem () -> ignore (f elem)) elements ()
-
-  let iteri_elements f elements =
-    List.fold_right
-      (fun elem offset ->
-         let () = f offset elem in
-         offset + String.length elem)
-      elements
-      0
-
-
-  let to_bytes buf =
-    let len = length buf in
-    let b = Bytes.create len in
-    let _ = List.fold_left
-        (fun i s ->
-           let l = String.length s in
-           Bytes.blit_string s 0 b (i - l) l;
-           i - 1)
-        len
-        buf.elements
-    in
-    b
+  let to_bytes buf = Immutable.to_bytes buf.elements
 
   let contents buf = Bytes.unsafe_to_string (to_bytes buf)
 
   let output_buffer outch {elements} =
-    iter_elements (output_string outch) elements
+    Immutable.output_buffer outch elements
 
-  let add_buffer l r = l.elements <- r.elements @ l.elements
+  let add_buffer l r =
+    l.elements <- Immutable.add_buffer r.elements l.elements
 end
+
+let formatter_of_aobuffer buf =
+  Format.make_formatter (Mutable.add_substring buf) ignore
