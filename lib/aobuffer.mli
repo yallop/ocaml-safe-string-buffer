2bb392b1f643eab847cfab880fba7732866203f0
Jeremy Yallop <yallop@gmail.com>
2015-10-23 23:49:19 +0100

    Group functions in interface.

--- lib/aobuffer.mli
+++ lib/aobuffer.mli
@@ -1,10 +1,12 @@
 type t
+
 val create : unit -> t
-val contents : t -> string
-val to_bytes : t -> bytes
+
 val length : t -> int
+
 val reset : t -> unit
 val clear : t -> unit
+
 val add_char : t -> char -> unit
 val add_string : t -> string -> unit
 val add_bytes : t -> bytes -> unit
@@ -12,6 +14,9 @@ val add_substring : t -> string -> int -> int -> unit
 val add_subbytes : t -> bytes -> int -> int -> unit
 val add_buffer : t -> Buffer.t -> unit
 val add_aobuffer : t -> t -> unit
+
 val output_buffer : out_channel -> t -> unit
+val contents : t -> string
+val to_bytes : t -> bytes
 
 val formatter_of_aobuffer : t -> Format.formatter
