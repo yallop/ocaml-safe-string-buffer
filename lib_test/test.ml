cb369271f52df5d56c2b4809f52a9fe1839251b7
Jeremy Yallop <yallop@gmail.com>
2015-10-24 11:28:03 +0100

    More tests.

--- lib_test/test.ml
+++ lib_test/test.ml
@@ -20,36 +20,87 @@ let test_create _ =
     assert_bool "create builds physically-distinct buffers"
       (buf1 != buf2);
 
-    let s = Aobuffer.add_string buf1 "x" in
+    let () = Aobuffer.add_string buf1 "x" in
 
     assert_equal "" (Aobuffer.contents buf2);
     assert_equal "x" (Aobuffer.contents buf1);
   end
-(*
-val contents : t -> string
-val to_bytes : t -> bytes
-val reset : t -> unit
-val clear : t -> unit
-val add_char : t -> char -> unit
-val add_string : t -> string -> unit
-val add_bytes : t -> bytes -> unit
-val add_substring : t -> string -> int -> int -> unit
-val add_subbytes : t -> bytes -> int -> int -> unit
-val add_buffer : t -> Buffer.t -> unit
-val add_aobuffer : t -> t -> unit
-val output_buffer : out_channel -> t -> unit
 
-val formatter_of_aobuffer : t -> Format.formatter
-                                 *)
 
-let suite = "Aobuffer tests" >:::
-  ["length"
+let test_add _ =
+  let buf = Aobuffer.create () in
+  begin
+    let () = Aobuffer.add_char buf 'a' in
+    assert_equal "a" (Aobuffer.contents buf);
+
+    let () = Aobuffer.add_string buf "bcd" in
+    assert_equal "abcd" (Aobuffer.contents buf);
+
+    let () = Aobuffer.add_bytes buf (Bytes.of_string "efg") in
+    assert_equal "abcdefg" (Aobuffer.contents buf);
+
+    let () = Aobuffer.add_substring buf "abcdefghijklmno" 7 4 in
+    assert_equal "abcdefghijk" (Aobuffer.contents buf);
+
+    let () = Aobuffer.add_subbytes buf (Bytes.of_string "abcdefghijklmno") 11 2 in
+    assert_equal "abcdefghijklm" (Aobuffer.contents buf);
+
+    let buf2 = Buffer.create 10 in
+    let () = Buffer.add_string buf2 "nopq" in
+    let () = Aobuffer.add_buffer buf buf2 in
+    assert_equal "abcdefghijklmnopq" (Aobuffer.contents buf);
+
+    let buf3 = Aobuffer.create () in
+    let () = Aobuffer.add_string buf3 "rst" in
+    let () = Aobuffer.add_aobuffer buf buf3 in
+    assert_equal "abcdefghijklmnopqrst" (Aobuffer.contents buf);
+  end
+
+
+let test_clear _ =
+  let buf = Aobuffer.create () in
+  let () = Aobuffer.add_string buf "abc" in
+  assert_equal "abc" (Aobuffer.contents buf);
+
+  let () = Aobuffer.clear buf in
+  assert_equal "" (Aobuffer.contents buf)
+
+  
+let test_reset _ =
+  let buf = Aobuffer.create () in
+  let () = Aobuffer.add_string buf "abc" in
+  assert_equal "abc" (Aobuffer.contents buf);
+
+  let () = Aobuffer.reset buf in
+  assert_equal "" (Aobuffer.contents buf)
+
+
+let test_formatter _ =
+  let buf = Aobuffer.create () in
+  let fmt = Aobuffer.formatter_of_aobuffer buf in
+  let () = Format.fprintf fmt "x%az@." (fun fmt -> Format.fprintf fmt "%d") 3 in
+  assert_equal "x3z\n" (Aobuffer.contents buf)
+
+
+let suite = "Aobuffer tests" >::: [
+    "length"
     >:: test_length;
-
-   "create"
+    
+    "create"
     >:: test_create;
-  ]
+    
+    "add"
+    >:: test_add;
 
+    "clear"
+    >:: test_clear;
+
+    "reset"
+    >:: test_reset;
+
+    "test_formatter"
+    >:: test_formatter;
+  ]
 
 
 let _ =
